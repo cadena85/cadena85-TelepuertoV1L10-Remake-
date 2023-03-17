5f8f204f54f7ca4ce5dace00faffd150b77aea41
Albert <humanoajeno@gmail.com>
2023-03-16 23:20:18 -0600

    consumo de servicio web con python(1)

---
+++ app/Http/Controllers/pySOAPController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SoapClient;
+class pySOAPController extends Controller
+{
+    //
+     public function consumirServicioPy(){
+        $client = new SoapClient("http://localhost:8000/?wsdl");
+        //dd($client);
+        $params = array(
+            'name' => "Rob Paper Sheet",
+            'times' => 7
+        );
+        $resultado = $client->say_hello($params);
+        $arreglo= $resultado->say_helloResult->string;
+        //dd($arreglo);
+        foreach ($arreglo as $key => $value) {
+            // code...
+            echo "<br>".$key.": ".$value;
+        }
+        
+     }
+
+}

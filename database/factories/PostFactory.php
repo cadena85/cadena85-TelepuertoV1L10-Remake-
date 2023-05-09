4f75a1628c0e8ea5286a22cc4e6923e679981e0e
Albert <humanoajeno@gmail.com>
2023-05-08 23:22:59 -0600

    Api REST Con Laravel inicio

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'user_id' => rand(1,10),//con 10 usarios
+            'title' => $this->faker->sentence,//oracion al azar
+            'slug' => $this->faker->slug,
+            'contenido' => $this->faker->text(500),//texto de 500 caracteres
+        ];
+    }
+}
